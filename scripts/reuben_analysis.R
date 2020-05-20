b8ab16751d1d3031e5b7d679c3f31144410cea13
ribble_T <rnarad@uw.edu>
2020-05-20 15:16:19 -0700

    added scripts directiry

---
+++ scripts/reuben_analysis.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(ggplot2)
+library(lintr)
+library(mapdata)
+library(maps)
+
+#Analysis of spotify top 50 dataset
+top_50_data <- read.csv("data/spotify_top50_by_country.csv")
+

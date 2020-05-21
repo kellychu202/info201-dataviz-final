e0d0c98f95cec40352fcb08c02c11c511352b69b
Kelly Chu <kellychu202@gmail.com>
2020-05-21 14:31:59 -0700

    set up aggregate table and chart scripts

---
+++ scripts/kelly_analysis.R
@@ -0,0 +1,5 @@
+# Chart script by Kelly
+# proper labels/titles/legends
+# intentional chart type and encoding selection based on the question of interest and data type
+# report: why this chart (what does it express)
+#         what does it reveal
